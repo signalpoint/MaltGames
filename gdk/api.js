68f5e838dedb8a9a93ad3a2638a974d2e726dcbe
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-21 23:14:32 +0000

    Add api.js for integration with api.php

---
+++ gdk/api.js
@@ -0,0 +1,12 @@
+Game.prototype.api = function(method, path, data, options) {
+  path = 'api.php?q=' + this.getKey() + '/' + path;
+  return this.xhr(method, path, data, options);
+};
+
+Game.prototype.apiGet = function(path, options) {
+  return this.api('GET', path, null, options);
+};
+
+Game.prototype.apiPost = function(path, data, options) {
+  return this.api('POST', path, data, options);
+};
