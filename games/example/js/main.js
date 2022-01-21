0e5a6c72739df0b973c99879aa9929936f074f1c
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-21 23:18:31 +0000

    Add example Game

---
+++ games/example/js/main.js
@@ -0,0 +1,58 @@
+// GLOBALS
+
+var game = null; // The Game object.
+
+function loadTheGame() {
+
+  // Initialize the game.
+  game = new Game('example');
+  game.setVersion('0.0.0');
+
+  // Connect to the web socket server and handle connection events...
+  game.connect({
+
+    // CONNECTION OPENED
+    open: function(e) {
+
+      game.toast('Connection opened!');
+
+    },
+
+    // MESSAGE RECEIVED
+    message: function(e) {
+
+      var data = JSON.parse(e.data);
+      console.log(data);
+
+      var op = data.op;
+      switch (op) {
+
+        default:
+          console.log('onmessage, unknown op: ' + op);
+          break;
+
+      }
+
+    },
+
+    // CONNECTION CLOSED
+    close: function(e) {
+      toastMessage('Connection closed!', 'danger');
+    },
+
+    // ERROR
+    error: function(e) {
+      toastMessage('Connection failed!', 'danger');
+    }
+
+  });
+
+}
+
+/**
+ * GAME PROTOTYPE EXTENSIONS
+ */
+
+Game.prototype.getContainer = function() {
+  return game.get('#myGameContainer');
+};
