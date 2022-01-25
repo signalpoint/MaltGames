c4168c366b87dce50536e12b32dc2e482533395a
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-25 03:16:18 +0000

    Add TODO to xhr. White space

--- gdk/xhr.js
+++ gdk/xhr.js
@@ -1,14 +1,17 @@
 Game.prototype.xhr = function(method, path, data, options) {
+
   return new Promise(function(resolve, reject) {
+
     var url = location.protocol + '//' + location.host + '/' + path;
+
     var req = new XMLHttpRequest();
     req.open(method, url);
     req.setRequestHeader('Content-type', 'application/json');
     req.onload = function() {
 //      console.log(req);
 
-//      var result = JSON.parse(req.responseText);
-
+      // TODO need to understand what is coming back (maybe need to use Accept header by client)
+      // that way we can properly parse the response here, e.g. JSON vs audio/mpeg
       var result = null;
       if (path.indexOf('sound') != -1) {
         result = req.response;
@@ -20,11 +23,17 @@ Game.prototype.xhr = function(method, path, data, options) {
       req.status === 200 ?
         resolve(result) :
         reject(new Error(result.error.code + ' - ' + result.error.msg));
+
     };
     req.onerror = function() {
+
       console.log(req);
       reject(new Error('xhr'));
+
     };
+
     data ? req.send(JSON.stringify(data)) : req.send();
+
   });
+
 };
