a4a8ca4398c2f35dbd923a76d621bd34f4931069
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-26 05:45:45 +0000

    Use a card-header on the games list

--- site/pages/games/games.tpl.php
+++ site/pages/games/games.tpl.php
@@ -1,16 +1,19 @@
 <div class="container mb-3">
 
   <!-- GAMES -->
-  <h2 class="visually-hidden">Games</h2>
+  <h1 class="visually-hidden">Games</h1>
   <div class="row row-cols-1 row-cols-md-2 g-4">
 
     <?php foreach (mkGames() as $key => $game) { ?>
     <div class="col">
-      <div class="card">
+      <div class="card text-dark bg-light">
+        <?php if (isset($game['slogan'])) { ?>
+        <div class="card-header"><?php print $game['slogan']; ?></div>
+        <?php } ?>
         <div class="card-body">
           <h3 class="card-title"><?php print $game['name']; ?></h3>
           <?php if (isset($game['slogan'])) { ?>
-          <h6 class="card-subtitle mb-2 text-muted"><?php print $game['slogan']; ?></h6>
+          <!--<h6 class="card-subtitle mb-2 text-muted"></h6>-->
           <?php } ?>
           <?php if (isset($game['description'])) { ?>
           <p class="card-text"><?php print $game['description']; ?></p>
