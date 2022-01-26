bf326d78b9feff56cfabd631973308fbb805098a
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-26 05:49:47 +0000

    Add mk Mod

---
+++ site/mods/mk/mk.php
@@ -0,0 +1,45 @@
+<?php
+
+function mkSocialNetworks() {
+
+  return [
+
+    'facebook' => [
+      'title' => 'Facebook',
+      'url' => 'https://www.facebook.com/MaltKit',
+      'handle' => 'MaltKit',
+    ],
+    'twitter' => [
+      'title' => 'Twitter',
+      'url' => 'https://twitter.com/TheMaltKit',
+      'handle' => 'TheMaltKit',
+    ],
+    'youtube' => [
+      'title' => 'YouTube',
+      'url' => 'https://www.youtube.com/channel/UCER1oQBRQSZsuS1qtWQwsbQ',
+      'handle' => NULL,
+    ],
+    'reddit' => [
+      'title' => 'Reddit',
+      'url' => 'https://www.reddit.com/r/MaltKit/',
+      'handle' => 'MaltKit',
+    ],
+    'instagram' => [
+      'title' => 'Instagram',
+      'url' => 'https://www.instagram.com/themaltkit/',
+      'handle' => 'themaltkit',
+    ],
+    'pinterest' => [
+      'title' => 'Pinterest',
+      'url' => 'https://www.pinterest.com/themaltkit',
+      'handle' => 'themaltkit',
+    ],
+    'github' => [
+      'title' => 'GitHub',
+      'url' => 'https://github.com/signalpoint/MaltGames',
+      'handle' => 'MaltGames',
+    ],
+
+  ];
+
+}
