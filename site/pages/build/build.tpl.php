aafec82295ac0a920c7c8f93b1ed661426392962
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-27 18:32:26 +0000

    Add simple list-group on build page as placeholders

--- site/pages/build/build.tpl.php
+++ site/pages/build/build.tpl.php
@@ -1,3 +1,11 @@
 <div class="container mb-3">
-  What do you want to build?
+
+  <p>What do you want to make?</p>
+
+  <ul class="list-group">
+    <li class="list-group-item">Games</li>
+    <li class="list-group-item">Apps</li>
+    <li class="list-group-item">Sites</li>
+  </ul>
+
 </div>
