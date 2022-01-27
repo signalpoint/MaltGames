88a366729121edb0968f19db07d33db88bf9f03f
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-27 18:45:20 +0000

    On Site, add variables and methods for themes, currentTheme, routes, and currentRoute. Also add loadMod(), initMods() and initMod(), and determineCurrentRoute().

--- src/Site.php
+++ src/Site.php
@@ -3,20 +3,32 @@
 namespace MaltGames;
 
 use MaltGames\Mod;
+use MaltGames\Theme;
+use MaltGames\Route;
 use MaltGames\Page;
+
+// TODO gotta figure out how to dynamically "use" this!
+use MaltKit\Mk;
 
 class Site {
 
     protected $mods;
+    protected $themes;
+    protected $currentTheme;
+    protected $routes;
+    protected $currentRoute;
     protected $pages;
-    protected $pageTemplates;
     protected $protocol;
     protected $domain;
     protected $baseUrl;
 
-    public function __construct() {
+    public function __construct($config) {
 
       $this->mods = [];
+      $this->themes = [];
+      $this->currentTheme = NULL;
+      $this->routes = [];
+      $this->currentRoute = NULL;
       $this->pages = [];
       $this->pageTemplates = [];
       $this->protocol = mkProtocol();
@@ -25,6 +37,7 @@ class Site {
 
     }
 
+    // MODS
     public function getMods() { return $this->mods; }
     public function addMods($mods) {
       foreach ($mods as $id => $mod) {
@@ -32,12 +45,122 @@ class Site {
       }
     }
     public function addMod($id, $mod) {
-      $this->mods[$id] = new Mod($id, $mod);
+//      $this->mods[$id] = new $mod['class']($id, $mod);
+      $this->mods[$id] = new Mk($id, $mod);
     }
-    public function loadMod($id) {
+    public function getMod($id) {
       return isset($this->mods[$id]) ? $this->mods[$id] : NULL;
     }
+    public function loadMod($id) {
+      if (isset($this->mods[$id])) {
+        $this->mods[$id]->load();
+      }
+    }
+    public function initMods($mods) {
 
+      // Add mods to site.
+      $this->addMods($mods);
+
+      // For each initialized mod on the site...
+      foreach ($this->getMods() as $id => $mod) {
+        $this->initMod($mod);
+      }
+
+    }
+    public function initMod($mod) {
+
+      // Load the mod.
+      $mod->load();
+
+      // Add its routes to the Site.
+      $routes = $mod->getRoutes();
+      if ($routes) { $this->addRoutes($routes); }
+
+    }
+
+    // THEMES
+    public function getThemes() { return $this->themes; }
+    public function addThemes($themes) {
+      foreach ($themes as $id => $theme) {
+        $this->addTheme($id, $theme);
+      }
+    }
+    public function addTheme($id, $theme) {
+      $this->themes[$id] = new Theme($id, $theme);
+    }
+    public function getTheme($id) {
+      return isset($this->themes[$id]) ? $this->themes[$id] : NULL;
+    }
+    public function getCurrentTheme() {
+      return $this->currentTheme;
+    }
+    public function setCurrentTheme($theme) {
+      $this->currentTheme = $theme;
+    }
+
+    // ROUTES
+    public function getRoutes() { return $this->routes; }
+    public function addRoutes($routes) {
+      foreach ($routes as $id => $route) {
+        $this->addRoute($id, $route);
+      }
+    }
+    public function addRoute($id, $route) {
+      $this->routes[$id] = new Route($id, $route);
+    }
+    public function getRoute($id) {
+      return isset($this->routes[$id]) ? $this->routes[$id] : NULL;
+    }
+    public function getCurrentRoute() {
+      return $this->currentRoute;
+    }
+    public function setCurrentRoute($route) {
+      $this->currentRoute = $route;
+    }
+
+    public function determineCurrentRoute() {
+
+      $currentPath = mkPath();
+
+      // If there isn't a current path, use the default route.
+      if (!$currentPath) {
+        return $this->getRoute(mkSiteConfig()['defaultRoute']);
+      }
+
+      // For each route, see if its path matches the current path, if so, we've
+      // got our route. Set it, and return it.
+      $route = NULL;
+      $routes = $this->getRoutes();
+      foreach ($routes as $key => $route) {
+        if ($route->getPath() == $currentPath) {
+          $this->setCurrentRoute($route);
+          return $route;
+        }
+      }
+
+      // We didn't find a route...
+
+      // Work backwords through the current path's args to locate a route with
+      // a matching path.
+      $args = explode('/', $currentPath);
+      while (count($args)) {
+        array_pop($args);
+        $path = implode('/', $args);
+        foreach ($routes as $key => $route) {
+          if ($route->getPath() == $path) {
+            $this->setCurrentRoute($route);
+            return $route;
+          }
+        }
+      }
+
+      $this->setCurrentRoute($route);
+
+      return $this->getCurrentRoute();
+
+    }
+
+    // PAGES
     public function addPages($pages) {
       foreach ($pages as $id => $page) {
         $this->addPage($id, $page);
@@ -54,14 +177,6 @@ class Site {
      */
     public function loadPage($id) {
       return isset($this->pages[$id]) ? $this->pages[$id] : NULL;
-    }
-
-    public function getPageTemplate($name) {
-      return isset($this->pageTemplates[$name]) ?
-      $this->pageTemplates[$name] : NULL;
-    }
-    public function setPageTemplate($name, $template) {
-      $this->pageTemplates[$name] = $template;
     }
 
     public function getProtocol() {
