04bbf8759899ddc95414c5c84c1b5c261288a9a0
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-27 20:38:33 +0000

    On Theme, add regions property and methods.

--- src/Theme.php
+++ src/Theme.php
@@ -5,15 +5,20 @@ namespace MaltGames;
 class Theme {
 
   protected $id;
+  protected $regions;
   protected $pageTemplates;
 
   public function __construct($id, $theme) {
 
     $this->id = $id;
+    $this->regions = [];
     $this->pageTemplates = [];
 
     if (isset($theme['pageTemplates'])) {
       $this->pageTemplates = $theme['pageTemplates'];
+    }
+    if (isset($theme['regions'])) {
+      $this->regions = $theme['regions'];
     }
 
   } // __construct
@@ -28,6 +33,17 @@ class Theme {
   public function getId() {
     return $this->id;
   }
+
+  // REGIONS
+
+  public function getRegions() {
+    return $this->regions;
+  }
+  public function getRegion($id) {
+    return isset($this->regions[$id]) ? $this->regions[$id] : NULL;
+  }
+
+  // PAGE TEMPLATES
 
   public function getPageTemplates() { return $this->pageTemplates; }
   public function addPageTemplates($pageTemplates) {
