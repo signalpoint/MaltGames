887ba0cc4e497910ec948a14878f6ea84a9c4da0
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-28 02:06:35 +0000

    Add docs pages

---
+++ site/pages/docs/docs.controller.php
@@ -0,0 +1,27 @@
+<?php
+
+function mkDocsTableOfContentsMenu() {
+  // TODO honestly the menu system should just be made up of Page objects, that
+  // way they have all the goodies with html,head,body,etc alongside them.
+  return [
+
+    // GAMES
+    'games' => [
+      'text' => 'Games',
+      'route' => 'mkDocs.games',
+    ],
+
+    // APPS
+    'apps' => [
+      'text' => 'Apps',
+      'route' => 'mkDocs.apps',
+    ],
+
+    // SITES
+    'sites' => [
+      'text' => 'Sites',
+      'route' => 'mkDocs.sites',
+    ],
+
+  ];
+}
---
+++ site/pages/docs/sites/sites.php
@@ -0,0 +1,5 @@
+<div class="container">
+
+  <p>Sites docs...</p>
+
+</div>
