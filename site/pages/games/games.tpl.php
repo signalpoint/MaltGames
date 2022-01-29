b537a0821dd7bb533014a0ba69df3cfbe9251318
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-29 19:07:58 +0000

    Update games page to work with Game class

--- site/pages/games/games.tpl.php
+++ site/pages/games/games.tpl.php
@@ -5,20 +5,22 @@
   <div class="row row-cols-1 row-cols-md-2 g-4">
 
     <?php foreach (mkGames() as $key => $game) { ?>
+    <?php
+      $game = mkGameLoad($key);
+      $hasSlogan = !!$game->getSlogan();
+      $hasDescription = !!$game->getDescription();
+    ?>
     <div class="col">
       <div class="card text-dark bg-light">
-        <?php if (isset($game['slogan'])) { ?>
-        <div class="card-header"><?php print $game['slogan']; ?></div>
+        <?php if ($hasSlogan) { ?>
+        <div class="card-header"><?php print $game->getSlogan(); ?></div>
         <?php } ?>
         <div class="card-body">
-          <h3 class="card-title"><?php print $game['name']; ?></h3>
-          <?php if (isset($game['slogan'])) { ?>
-          <!--<h6 class="card-subtitle mb-2 text-muted"></h6>-->
+          <h3 class="card-title"><?php print $game->getName(); ?></h3>
+          <?php if ($hasDescription) { ?>
+          <p class="card-text"><?php print $game->getDescription(); ?></p>
           <?php } ?>
-          <?php if (isset($game['description'])) { ?>
-          <p class="card-text"><?php print $game['description']; ?></p>
-          <?php } ?>
-          <a href="<?php print $game['url']; ?>" class="card-link" title="Play <?php print $game['name']; ?>">Play</a>
+          <a href="<?php print $game->getUrl(); ?>" class="card-link" title="Play <?php print $game->getName(); ?>">Play</a>
         </div>
       </div>
     </div>
