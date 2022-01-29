6d61c6ee38d4d0475a833c60c0afff40fb90a241
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-29 19:06:07 +0000

    Let Game constructors handle game instantiation details

--- site/mods/MkGames/mkGames.php
+++ site/mods/MkGames/mkGames.php
@@ -3,23 +3,25 @@
 // For each Game...
 foreach (mkGames() as $game) {
 
-  // Add a Mod for the Game.
-  $gameModPath = 'sites/games/' . $game['key'];
+  $gameModPath = 'site/games/' . $game['key'];
+  $gameFiles = [
+    $gameModPath . "/{$game['key']}.php",
+  ];
+  if (isset($game['files'])) {
+    foreach ($game['files'] as $i => $file) {
+      $game['files'][$i] = $gameModPath . '/' . $file;
+    }
+    $gameFiles = array_merge($gameFiles, $game['files']);
+  }
+
+  // Add the Mod from the Game to the Site.
   $GLOBALS['site']->addMod($game['key'], [
     'namespace' => 'MaltKit',
     'class' => $game['name'],
     'name' => $game['name'],
     'path' => $gameModPath,
-    'description' => $game['description'],
-    'files' => [
-      $gameModPath . "/{$game['key']}.php",
-    ],
-  ]);
-
-  // Add a Route for the Game.
-  $GLOBALS['site']->addRoute($game['key'], [
-    'path' => 'games/' . $game['key'],
-    'page' => $game['page'],
+    'files' => $gameFiles,
+    'url' => $game['url'],
   ]);
 
 }
@@ -39,98 +41,34 @@ function mkGames() {
 
     'color-picker' => [
       'name' => 'ColorPicker',
-      'v' => '0.0.0',
-      'slogan' => 'Learn Colors in Another Language',
-      'description' => 'See, hear and touch colors to quickly learn the names of colors.',
-      'page' => [
-        'head' => [
-          'scripts' => [
-            'main.js',
-            'brain.js',
-            'widgets/the-color-to-learn.js',
-            'widgets/colored-buttons.js',
-            'widgets/score-board.js',
-          ],
-        ],
-      ],
     ],
 
     'number-pad' => [
       'name' => 'NumberPad',
-      'v' => '0.0.0',
-      'slogan' => 'Learn Numbers in Another Language',
-      'description' => 'See, hear and touch numbers to quickly learn zero through nine.',
-      'page' => [
-        'head' => [
-          'scripts' => [
-            'main.js',
-            'brain.js',
-            'widgets/the-number-to-learn.js',
-            'widgets/dial-pad.js',
-            'widgets/score-board.js',
-          ],
-        ],
-      ],
-      'api' => [
-        'numbers' => [
-          'get' => [],
-        ],
-        'sound' => [
-          'get' => [
-            'header' => [
-              'Content-Type' => 'audio/mpeg'
-            ],
-          ],
-        ],
-      ],
     ],
 
     'object-hunt' => [
       'name' => 'ObjectHunt',
-      'v' => '0.0.0',
-      'slogan' => 'Learn Objects in Another Language',
-      'description' => 'Hunt for objects and their matching translation.',
-      'page' => [
-        'head' => [
-          'scripts' => [
-            'main.js',
-            'brain.js',
-            'classes/card.js',
-          ],
-        ],
-      ],
-      'api' => [
-        'objects' => [
-          'get' => [],
-        ],
-        'sound' => [
-          'get' => [
-            'header' => [
-              'Content-Type' => 'audio/mpeg'
-            ],
-          ],
-        ],
-      ],
     ],
-
-    'word-dash' => [
-      'name' => 'WordDash',
-      'v' => '0.0.0',
-      'slogan' => 'Translation Race',
-      'description' => 'A race to translate as many words as you can.',
-      'page' => [
-        'head' => [
-          'scripts' => [
-            'chat.js',
-            'main.js',
-            'play.js',
-            'player.js',
-            'players.js',
-            'words.js',
-          ],
-        ],
-      ],
-    ],
+//
+//    'word-dash' => [
+//      'name' => 'WordDash',
+//      'v' => '0.0.0',
+//      'slogan' => 'Translation Race',
+//      'description' => 'A race to translate as many words as you can.',
+//      'page' => [
+//        'head' => [
+//          'scripts' => [
+//            'chat.js',
+//            'main.js',
+//            'play.js',
+//            'player.js',
+//            'players.js',
+//            'words.js',
+//          ],
+//        ],
+//      ],
+//    ],
 
   ];
 
@@ -144,16 +82,10 @@ function mkGames() {
 }
 
 /**
- * Returns a game.
+ * Returns a Game.
  * @param string $key
- * @return array
+ * @return Game
  */
 function mkGameLoad($key) {
-  $games = mkGames();
-  return isset($games[$key]) ? $games[$key] : NULL;
-}
-
-function mkGameLoadApi($key) {
-  $game = mkGameLoad($key);
-  return isset($game['api']) ? $game['api'] : NULL;
+  return $GLOBALS['site']->getMod($key);
 }
