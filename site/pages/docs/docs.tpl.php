4a55e64866085fcdacbf448bb2f868c25df2ef31
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-29 03:50:16 +0000

    Add slogan to docs page.

--- site/pages/docs/docs.tpl.php
+++ site/pages/docs/docs.tpl.php
@@ -33,9 +33,13 @@
 
     </div>
 
-    <div class="col-12 col-md-10">Docs!</div>
+    <div class="col-12 col-md-10">
+
+      <p>A tool box for learning modern coding technologies and building your
+        own custom games, applications and websites, for all devices.</p>
+
+    </div>
 
   </div>
-
 
 </div>
