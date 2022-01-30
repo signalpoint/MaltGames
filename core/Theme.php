fa70568fe7aebd110e250720ea450ccd5430cce8
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-01-30 18:16:12 +0000

    In Theme, add namespace, class, name, path, description and files properties and methods, and add addRegions() and addRegion()

--- core/Theme.php
+++ core/Theme.php
@@ -5,6 +5,14 @@ namespace MaltKit;
 class Theme {
 
   protected $id;
+
+  public $namespace;
+  public $class;
+  public $name;
+  public $path;
+  public $description;
+  public $files;
+
   public $regions;
   public $pageTemplates;
   public $contentTemplates;
@@ -16,15 +24,16 @@ class Theme {
     $this->pageTemplates = [];
     $this->contentTemplates = [];
 
-    if (isset($theme['regions'])) {
-      $this->regions = $theme['regions'];
-    }
-    if (isset($theme['pageTemplates'])) {
-      $this->pageTemplates = $theme['pageTemplates'];
-    }
-    if (isset($theme['contentTemplates'])) {
-      $this->contentTemplates = $theme['contentTemplates'];
-    }
+    if (isset($theme['namespace'])) { $this->namespace = $theme['namespace']; }
+    if (isset($theme['class'])) { $this->class = $theme['class']; }
+    if (isset($theme['name'])) { $this->name = $theme['name']; }
+    if (isset($theme['path'])) { $this->path = $theme['path']; }
+    if (isset($theme['description'])) { $this->description = $theme['description']; }
+    if (isset($theme['files'])) { $this->files = $theme['files']; }
+
+    if (isset($theme['regions'])) { $this->regions = $theme['regions']; }
+    if (isset($theme['pageTemplates'])) { $this->pageTemplates = $theme['pageTemplates']; }
+    if (isset($theme['contentTemplates'])) { $this->contentTemplates = $theme['contentTemplates']; }
 
   } // __construct
 
@@ -34,16 +43,48 @@ class Theme {
     return $this->getId();
   }
 
-  // METHODS...
+  // METHODS
 
   public function getId() {
     return $this->id;
+  }
+
+  public function getNamespace() {
+    return $this->namespace;
+  }
+
+  public function getClass() {
+    return $this->class;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function getPath() {
+    return $this->path;
+  }
+
+  public function getDescription() {
+    return $this->description;
+  }
+
+  public function getFiles() {
+    return $this->files;
   }
 
   // REGIONS
 
   public function getRegions() {
     return $this->regions;
+  }
+  public function addRegions($regions) {
+    foreach ($regions as $id => $region) {
+      $this->addRegion($id, $region);
+    }
+  }
+  public function addRegion($id, $region) {
+    $this->regions[$id] = $region;
   }
   public function getRegion($id) {
     return isset($this->regions[$id]) ? $this->regions[$id] : NULL;
@@ -57,7 +98,6 @@ class Theme {
       $this->addPageTemplate($id, $pageTemplate);
     }
   }
-
   public function addPageTemplate($id, $pageTemplate) {
     $this->pageTemplates[$id] = $pageTemplate;
   }
@@ -65,7 +105,7 @@ class Theme {
     return isset($this->pageTemplates[$id]) ? $this->pageTemplates[$id] : NULL;
   }
 
-  // CURRENT PAGE TEMPLATES
+  // CURRENT PAGE TEMPLATE
 
   public function getCurrentPageTemplate() {
     return $this->currentPageTemplate;
@@ -88,6 +128,26 @@ class Theme {
   }
   public function addContentTemplate($id, $contentTemplate) {
     $this->contentTemplates[$id] = $contentTemplate;
+  }
+
+  /**
+   *
+   * INTERFACES
+   *
+   *    Implement these when you extend Theme.
+   *
+   */
+
+  public function regions() {
+    return NULL;
+  }
+
+  public function pageTemplates() {
+    return NULL;
+  }
+
+  public function contentTemplates() {
+    return NULL;
   }
 
 }
