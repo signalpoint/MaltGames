43d99a9c9ac552c2d541c8ad1463bc0e0029f9dd
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-02-09 15:19:05 +0000

    Move cdk code to src dir and add package.json and gulpfile.js to compile maltkit-cdk.js from src

---
+++ core/cdk/gulpfile.js
@@ -0,0 +1,48 @@
+var makeBinary = true;
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    gp_concat = require('gulp-concat'),
+    gp_rename = require('gulp-rename'),
+    gp_uglify = require('gulp-uglify');
+
+var jsSrc = [
+
+  './src/stopwatch.js',
+  './src/animation-timer.js',
+
+  './src/CanvasGame.js',
+  './src/MkEntity.js',
+  './src/Circle.js',
+  './src/Polygon.js',
+  './src/MkPoint.js',
+  './src/MkPolygon.js',
+  './src/Square.js',
+  './src/MkImage.js',
+  './src/MkSprite.js',
+
+];
+
+// Minify JavaScript
+function minifyJs() {
+  console.log('compressing maltkit-cdk.js...');
+  var js = gulp.src(jsSrc)
+    .pipe(gp_concat('maltkit-cdk.js'))
+    .pipe(gulp.dest('./'));
+//  if (makeBinary) {
+//    console.log('compressing maltkit-cdk.min.js...');
+//    return js.pipe(gp_rename('maltkit-cdk.min.js'))
+//    .pipe(gp_uglify())
+//    .pipe(gulp.dest('./'));
+//  }
+  return js;
+}
+gulp.task(minifyJs);
+
+gulp.task('default', function(done) {
+
+  gulp.watch(jsSrc, gulp.series('minifyJs'));
+
+  done();
+
+});
