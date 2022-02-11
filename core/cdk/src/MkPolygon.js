4ca66f70bf9ecc66ab0ca1d8406e3210c3d7fb46
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-02-11 03:38:38 +0000

    Add MkPolygon

---
+++ core/cdk/src/MkPolygon.js
@@ -0,0 +1,81 @@
+class MkPolygon extends MkEntity {
+
+  // PROPERTIES
+  //
+  //  radius
+  //  sides
+  //  startAngle
+  //
+  //  points
+  //
+
+  constructor(id, entity) {
+    super(id, 'MkPolygon', entity);
+  }
+
+  addPoints() {
+
+    if (!this.points) {
+
+      this.points = [];
+
+      var angle = this.startAngle || 0;
+      for (var i = 0; i < this.sides; i++) {
+        this.points.push(new MkPoint(
+          this.x + this.radius * Math.sin(angle),
+          this.y - this.radius * Math.cos(angle)
+        ));
+        angle += 2 * Math.PI / this.sides;
+      }
+
+    }
+
+    return this.points.length ? this.points : null;
+
+  }
+
+  createPath() {
+
+    this.addPoints();
+    var points = this.points;
+
+    context.beginPath();
+    context.moveTo(points[0].x, points[0].y);
+
+    for (var i = 1; i < this.sides; i++) {
+      context.lineTo(points[i].x, points[i].y);
+    }
+
+    context.closePath();
+
+  }
+
+  stroke() {
+    context.save();
+    this.createPath();
+    context.strokeStyle = this.strokeStyle;
+    context.stroke();
+    context.restore();
+  }
+
+  fill() {
+    context.save();
+    this.createPath();
+    context.fillStyle = this.fillStyle;
+    context.fill();
+    context.restore();
+  }
+
+  move(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    context.beginPath();
+    this.createPath();
+    this.stroke();
+    this.fill();
+  }
+
+}
