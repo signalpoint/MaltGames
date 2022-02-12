7fb9858e0b679c493fa8d54dcfa8a0e388d47ea9
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-02-12 02:53:55 +0000

    Add MkRectangle

---
+++ core/cdk/src/MkRectangle.js
@@ -0,0 +1,40 @@
+
+class MkRectangle extends MkEntity {
+
+  // PROPERTIES
+  //
+  //  width
+  //  height
+  //
+
+  constructor(id, entity) {
+    super(id, 'MkRectangle', entity);
+  }
+
+  createPath() {
+    context.beginPath();
+    context.moveTo(this.x, this.y);
+    context.lineTo(this.x + this.width, this.y);
+    context.lineTo(this.x + this.width, this.y + this.height);
+    context.lineTo(this.x, this.y + this.height);
+//    context.strokeRect(this.x, this.y, this.width, this.height);
+    context.closePath();
+  }
+
+  draw() {
+
+    context.save();
+    this.createPath();
+//    context.strokeStyle = this.strokeStyle;
+    context.stroke();
+//    context.fillStyle = this.fillStyle;
+    context.fill();
+    context.restore();
+
+//    context.beginPath();
+//    context.moveTo(RUBBER.origin.x, RUBBER.origin.y);
+//    context.strokeRect(RUBBER.shape.x, RUBBER.shape.y, RUBBER.shape.width, RUBBER.shape.height);
+//    context.stroke();
+  }
+
+}
